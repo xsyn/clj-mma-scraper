f2f829a0fb8c1f172fb7fb62c75c6ecc66bca642
Guy Taylor <guy.taylor@gmail.com>
2015-12-10 17:52:25 +0200

    Added fighters

--- clj-mma-scraper/src/clj_mma_scraper/core.clj
+++ clj-mma-scraper/src/clj_mma_scraper/core.clj
@@ -37,6 +37,9 @@
 
 
 ;; Setup scraper
+
+;; TODO: Clean up try+ catch, use try catch instead
+
 (defn get-url [url]
   (try+
    (client/get url {:insecure true})
@@ -58,7 +61,13 @@
         (html/html-resource (java.io.StringReader.
                              (:body check-url))))))
 
-;; Utility functions
+;; Standard Utility functions
+
+(defn format-date [datestring]
+  "Turns 'November 29, 2015' into #inst date format for datomic"
+  (c/to-date (tf/parse (tf/formatter "MMMM dd, YYYY") datestring)))
+
+;; HTML Utility functions
 
 (defn html-select-first [row tree]
   (first (html/select row tree)))
@@ -66,30 +75,52 @@
 (defn get-href [element]
   (get-in element [:attrs :href]))
 
+(defn get-data-link [element]
+  (get-in element [:attrs :data-link]))
+
 (defn get-content [element path]
   (str/trim (first (:content (html-select-first element path)))))
 
+(defn clean-weight-division [weight-string]
+  (str/trim (str/replace weight-string #"Bout" "")))
+
 (defn extract-type-from-name [name]
-  "Handles "
   (cond (.contains name "on Fox") :event.type/fox
         (.contains name "Ultimate Fighter") :event.type/ultimatefighter
         (.contains name "Fight Night") :event.type/fightnight
         :else :event.type/ufc))
 
-(defn format-date [datestring]
-  "Turns 'November 29, 2015' into #inst date format for datomic"
-  (c/to-date (tf/parse (tf/formatter "MMMM dd, YYYY") datestring)))
+(defn extract-type-from-weight [weight]
+  (cond (= weight "Women's Strawweight") :fight.weightclass/womansstrawweight
+        (= weight "Flyweight") :fight.weightclass/flyweight
+        (= weight "Bantamweight") :fight.weightclass/bantamweight
+        (= weight "Women's Bantamweight") :fight.weightclass/womensbantamweight
+        (= weight "Featherweight") :fight.weightclass/featherweight
+        (= weight "Lightweight") :fight.weightclass/lightweight
+        (= weight "Welterweight") :fight.weightclass/welterweight
+        (= weight "Middleweight") :fight.weightclass/middleweight
+        (= weight "Light Heavyweight") :fight.weightclass/lightheavyweight
+        :else :fight.weightclass/heavyweight))
+
 
 ;; CRUD functions
 
 (defn insert-event [event-name event-url event-date event-location event-type]
   "Transaciton to insert events into Datomic"
-  @(d/transact conn [{:db/id #db/id[:db.part/db]
-                      :event/name event-name
-                      :event/url event-url
-                      :event/date event-date
-                      :event/location event-location
-                      :event/type event-type}]))
+  (try
+    @(d/transact conn [{:db/id #db/id[:db.part/db]
+                        :event/name event-name
+                        :event/url event-url
+                        :event/date event-date
+                        :event/location event-location
+                        :event/type event-type}])
+    (catch Throwable t nil)))
+
+(defn insert-fighter [m]
+  "Insert a fighter map into datomic")
+
+(defn insert-fights [fight-url fight-stats fight-weightclass fight-time fight-rounds fight-method fight-winner fight-loser]
+  "Transaction to insert fights into Datomic")
 
 ;; Scraping stuff
 
@@ -114,12 +145,59 @@
                               event-location
                               (extract-type-from-name event-name)))))
 
+;; Event url
+(def event-url "http://www.fightmetric.com/event-details/4d636c3aa1105950")
 
+(defn get-fight-items [body]
+  "Passing in an html body, returns a map of fight-items, including round, time, time format, referee and details"
+  (let [b (html/select body [:i.b-fight-details__text-item])]
+    (vec (map (fn [x] (str/trim (nth (:content x) 2))) b))))
+
+(defn get-fighter [url]
+  "Gets a fighters details from a url")
+
+(defn map-fighters [body]
+  "Gets information about the fighter in a fight"
+  (map (fn [row] (let [outcome (str/trim (first (:content (first (html/select row [:div :i])))))
+                      name (first (:content (first (html/select row [:div :div :h3 :a]))))
+                      fighter-url (get-in (first (html/select row [:div :div :h3 :a])) [:attrs :href])]
+                  (zipmap [:outcome :name :url] [outcome name fighter-url]))) body))
+
+;; TODO: Get fighters and stats
+(defn get-fight [url]
+  "Get's fight details from a url"
+  (cond (nil? url) nil
+        :else
+        (let [body (html-select-first (fetch-body url) [:div.b-fight-details])
+              person-body (html/select body [:div.b-fight-details__person])
+              details-body (html-select-first body [:div.b-fight-details__fight])
+              weightclass (clean-weight-division (get-content body [:div.b-fight-details__fight-head :i]))
+              fighters (get-fighters person-body)
+              fi (get-fight-items details-body)
+              rounds (first fi)
+              time (second fi)
+              time-format (nth fi 2)
+              referee (nth fi 3)
+              method (str/trim (first (:content (second (html/select details-body [:i.b-fight-details__text-item_first :i]
+
+                                                                     )))))])))
+(defn scrape-links-from-card [url]
+  "Returns a seq of links, to the actual fights from a card"
+  (cond (nil? url) nil
+        :else
+        (map get-data-link (html/select (fetch-body url) [:tbody :tr]))))
 
 ;; Complete crawl
-(-> (map insert-event-metadata)
-    (get-all-events)
-    events-url)
+;;
+;;
+;;
+(;;-> (map insert-event-metadata)
+ ;;  (get-all-events)
+ ;;  events-url
+ )
+
+
+
 
 
 
