a41a4c150b6f3821fadd342d8a9338049f032926
Guy Taylor <guy.taylor@gmail.com>
2015-12-21 17:49:32 +0200

    Working incremental save of events

--- clj-mma-scraper/src/clj_mma_scraper/core.clj
+++ clj-mma-scraper/src/clj_mma_scraper/core.clj
@@ -5,6 +5,7 @@
             [clj-time.format :as tf]
             [clj-time.coerce :as c]
             [clojure.string :as str]
+            [clojure.set :as st]
             [clojure.java.io :as io]
             [clj-http.client :as client]
             [clj-http.conn-mgr :as conn-mgr]
@@ -19,22 +20,31 @@
 
 
 ;; Some initial definitions
-(def events-url "http://www.fightmetric.com/statistics/events/completed?page=all")
+(def starting-url "http://www.fightmetric.com/statistics/events/completed?page=all")
 
 (def uri "datomic:free://localhost:4334/mma-stats")
 
-(def conn (d/connect uri))
-
 ;; Create database
+
+;; NB: Initializedb on the my dev version of Datomic is broken due to:https://groups.google.com/forum/#!topic/datomic/1WBgM84nKmc
 (defn initialize-db [uri]
   "Drops the database if it exists. Creats a new DB with schema"
   (if (false? (d/create-database uri))(do (d/delete-database uri)
                                           (initialize-db uri))
       (let [schema-tx (read-string (slurp "resources/schema.edn"))]
-        (do
+        (try
           (d/create-database uri)
-          (d/transact (d/connect uri) schema-tx)))))
+          (d/transact (d/connect uri) schema-tx)
+          (catch Throwable t nil)))))
 
+(defn conn [] (d/connect uri))
+
+(defn db [] (d/db conn))
+
+(def schema-tx (read-string (slurp "resources/schema.edn")))
+
+(defn init-db [uri]
+  (d/transact (d/connect uri) schema-tx))
 
 ;; Setup scraper
 
@@ -102,103 +112,183 @@
         (= weight "Light Heavyweight") :fight.weightclass/lightheavyweight
         :else :fight.weightclass/heavyweight))
 
+(defn get-text-first [element path]
+  (str/trim (html/text (first (html/select element path)))))
+
+(defn clean-detail-string [s]
+  (str/trim (second (str/split s #":"))))
+
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s )))
 
 ;; CRUD functions
 
-(defn insert-event [event-name event-url event-date event-location event-type]
-  "Transaciton to insert events into Datomic"
-  (try
-    @(d/transact conn [{:db/id #db/id[:db.part/db]
-                        :event/name event-name
-                        :event/url event-url
-                        :event/date event-date
-                        :event/location event-location
-                        :event/type event-type}])
-    (catch Throwable t nil)))
-
-(defn insert-fighter [m]
-  "Insert a fighter map into datomic")
-
-(defn insert-fights [fight-url fight-stats fight-weightclass fight-time fight-rounds fight-method fight-winner fight-loser]
-  "Transaction to insert fights into Datomic")
+(defn get-all-event-urls[]
+  "Get all the urls that are in the db"
+  (d/q '[:find [?url ...]
+         :in $
+         :where [_ :event/url ?url]] (d/db (conn))))
 
 ;; Scraping stuff
 
-(defn get-all-events [url]
-  "Pulls the event table from FightMetric"
-  (let [body (fetch-body url)]
-    (cond (nil? body) nil
-          :else (rest (html/select body [:table.b-statistics__table-events :tbody :tr.b-statistics__table-row])))))
+(defn make-event-tx [m]
+  (assoc m :db/id (d/tempid :db.part/user)))
 
-
-;; This function may be worth splitting up
-(defn insert-event-metadata [row]
-  "Takes FightMetric event table, iterates through trs, scrapes data, sanitizes and then inputs into Datomic."
-  (cond (nil? row) nil
-        :else (let [event-url (get-href (html-select-first row [:tr :> [:td (html/nth-of-type 1)] :i :a]))
-                    event-name (get-content row [:tr :> [:td (html/nth-of-type 1)] :i :a])
-                    event-date (get-content row [:tr :> [:td (html/nth-of-type 1)] :i :span])
-                    event-location (get-content row  [:tr :> [:td (html/nth-of-type 2)]])]
-                (insert-event event-name
-                              (URI. event-url)
-                              (format-date event-date)
-                              event-location
-                              (extract-type-from-name event-name)))))
-
-;; Event url
-(def event-url "http://www.fightmetric.com/event-details/4d636c3aa1105950")
+;; TODO: Get attendance string->int working
+(defn get-event-details [event-url]
+  "Takes in a url that maps to a FightMetric event, and creates a map of it's data."
+  (when event-url
+    (let [b (html/select (fetch-body event-url) [:div.l-page__container])
+          event-name (get-text-first b [:h2 :span])
+          ;; use positional destructuring to pull out values
+          ;; from 0, 1, 2 indexes of vector
+          [event-date
+           event-location
+           event-attendance] (->> (html/select b [:div.b-fight-details :div :ul :li])
+                                  (map html/text)
+                                  (map clean-detail-string))]
+      {:event/name event-name
+       :event/url event-url
+       :event/date (format-date event-date)
+       :event/location event-location
+       :event/type (extract-type-from-name event-name)
+                                        ;:event/attendance (parse-int (str/replace event-attendance
+                                        ;#"," ""))
+       })))
 
 (defn get-fight-items [body]
   "Passing in an html body, returns a map of fight-items, including round, time, time format, referee and details"
   (let [b (html/select body [:i.b-fight-details__text-item])]
     (vec (map (fn [x] (str/trim (nth (:content x) 2))) b))))
 
+(defn get-stat-items [body]
+  (let [b] (html/select body [:section.b-fight-details__section.js-fight-section :table :tbody :tr])))
+
 (defn get-fighter [url]
-  "Gets a fighters details from a url")
+  "Gets a fighters details from a url"
+  )
 
 (defn map-fighters [body]
   "Gets information about the fighter in a fight"
-  (map (fn [row] (let [outcome (str/trim (first (:content (first (html/select row [:div :i])))))
-                      name (first (:content (first (html/select row [:div :div :h3 :a]))))
-                      fighter-url (get-in (first (html/select row [:div :div :h3 :a])) [:attrs :href])]
-                  (zipmap [:outcome :name :url] [outcome name fighter-url]))) body))
+  (map (fn [row]
+         (let [outcome (str/trim (first (:content (first (html/select row [:div :i])))))
+               name (first (:content (first (html/select row [:div :div :h3 :a]))))
+               fighter-url (get-in (first (html/select row [:div :div :h3 :a])) [:attrs :href])]
+           (zipmap [:outcome :name :url] [outcome name fighter-url]))) body))
+
+(defn map-stats [fighters fight-url]
+  "Take a seq of fighters and looks for their stats, on a particular fight page."
+  (map (fn [fighter] ) fighters))
 
 ;; TODO: Get fighters and stats
 (defn get-fight [url]
   "Get's fight details from a url"
-  (cond (nil? url) nil
-        :else
-        (let [body (html-select-first (fetch-body url) [:div.b-fight-details])
-              person-body (html/select body [:div.b-fight-details__person])
-              details-body (html-select-first body [:div.b-fight-details__fight])
-              weightclass (clean-weight-division (get-content body [:div.b-fight-details__fight-head :i]))
-              fighters (get-fighters person-body)
-              fi (get-fight-items details-body)
-              rounds (first fi)
-              time (second fi)
-              time-format (nth fi 2)
-              referee (nth fi 3)
-              method (str/trim (first (:content (second (html/select details-body [:i.b-fight-details__text-item_first :i]
+  (when url
+    (let [body (html-select-first (fetch-body url) [:div.b-fight-details])
+          person-body (html/select body [:div.b-fight-details__person])
+          details-body (html-select-first body [:div.b-fight-details__fight])
+          weightclass (clean-weight-division (get-content body [:div.b-fight-details__fight-head :i]))
+          fighters (map-fighters person-body)
+          stats (map-stats fighters)
+          [rounds
+           time
+           time-format
+           referee] (get-fight-items details-body)
+          method (str/trim (first (:content (second (html/select details-body [:i.b-fight-details__text-item_first :i])))))])))
 
-                                                                     )))))])))
+;; NOTE: Need to handle edge-cases for draw
+
+;; Build a map of all urls that we'll need to populate
+
+(defn clean-fighter-row [row]
+  "Traverse the row to the correct attributes, flatten, pull out the links and remove nil"
+  (remove nil?
+          (map get-href
+               (flatten
+                (map :content (:content row))))))
+
+(defn get-fighters-url-from-card-body [body]
+  "This is a shockingly ugly piece of code that traverses trees of data to get to specifc end points "
+  (when body
+    (map clean-fighter-row
+         (html/select body [:tbody :tr [:td (html/nth-of-type 2)]]))))
+
 (defn scrape-links-from-card [url]
-  "Returns a seq of links, to the actual fights from a card"
-  (cond (nil? url) nil
-        :else
-        (map get-data-link (html/select (fetch-body url) [:tbody :tr]))))
-
-;; Complete crawl
+  "Returns a map of links, to the actual fights from a card, attached to fights. This could be two function, but I want to make a single call to the url."
+  (when url
+    (let [body (fetch-body url)]
+      (interleave
+       (map #(hash-map :fight %)
+            (map get-data-link (html/select body [:tbody :tr])))
+       (map #(hash-map :fighters %)
+            (get-fighters-url-from-card-body body))))))
 ;;
+;; TODO: (mapv #(-> (into {} %)) (partition 2 (scrape-links-from-card "http://www.fightmetric.com/event-details/e670f8cc2969a789")))
 ;;
-;;
-(;;-> (map insert-event-metadata)
- ;;  (get-all-events)
- ;;  events-url
- )
 
 
 
+(defn get-all-events [url]
+  "Returns a collection of event-urls, which are of all the UFC events ever."
+  (let [body (fetch-body url)]
+    (when body
+      (let [b (rest (html/select body [:table.b-statistics__table-events :tbody :tr.b-statistics__table-row]))]
+        (map (fn [row] (get-href
+                       (html-select-first row [:tr :> [:td (html/nth-of-type 1)] :i :a])))
+             b)))))
+
+(defn create-url-map []
+  "Create a tree of urls, which will contain the universe of FightMetric's urls that are important to me"
+  (let [starting-url events-url
+        events-list (get-all-events starting-url)
+        fight-list (map scrape-links-from-card events-list)]
+    (interleave events-list fight-list)))
+
+(defn get-and-insert-events [urls]
+  (->>
+   (map get-event-details urls)
+   (remove nil?)
+   (map make-event-tx)
+   (d/transact (conn))
+   deref
+   :db-after))
+
+(defn seed-events [starting-url]
+  "Inserts all events into db for the first time, retuns a list of urls and a db"
+  (->>
+   (get-all-events starting-url)
+   (get-and-insert-events)
+   (d/q '[:find ?e :where [?e :event/url ?url]])))
+
+(defn db-current? []
+  "Checks the difference between the events on system, and the events on FightMetric, inserts the difference."
+  (let [diff (st/difference
+              (set (get-all-events starting-url))
+              (set (get-all-event-urls)))]
+    (if (not-empty diff)
+      (->>
+       (get-and-insert-events diff)
+       (d/q '[:find [?url ...] :where [?e :event/url ?url]]))
+      (get-all-event-urls))))
+
+(comment
+  (defn insert-data []
+    (->>
+     (db-current?)
+     (map scrape-links-from-card))))
 
 
 
+;; 1. Initialize database
+;;    - Bug in Datomic. TODO: Check transactors are up to date.
+;; 2. Check for new events
+;; 3. If there are new events insert them into db
+;;    - Get events url - DONE
+;;    - Get events details - DONE
+;;    - Insert events - DONE
+;; 4. Validate fights on events
+;; 5. Validate fighters on fights
 
+;; (get-all-events)
+;; (is there a difference between events in the db and events in the
+;; list?, if so insert new ones, and return all-event-urls from db)
